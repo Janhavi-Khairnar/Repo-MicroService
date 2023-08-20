9adbf518f3facccef393f01dae2fbd788dbeba99
Janhavi-Khairnar <khairnarj2000@gmail.com>
2023-08-20 10:47:25 +0530

    [ADDED]:[Janhavi] Creating API in Customer Controller By Using Rest Template Concept

--- src/main/java/com/igc/customerservice/controller/CustomerController.java
+++ src/main/java/com/igc/customerservice/controller/CustomerController.java
@@ -1,9 +1,11 @@
 package com.igc.customerservice.controller;
 
 import com.igc.customerservice.entity.Customer;
+import com.igc.customerservice.entity.Orders;
 import com.igc.customerservice.repo.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -13,6 +15,9 @@ public class CustomerController {
 
     @Autowired
     CustomerRepo customerRepo;
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @PostMapping("/addCustomer")
     public Customer addCustomer(@RequestBody Customer customer) {
@@ -28,4 +33,19 @@ public class CustomerController {
     public  Customer getcustomerbyid(@PathVariable ("cid") int cid) {
         return customerRepo.findById(cid).get();
     }
+
+    @GetMapping("/getcustomerbycid/{cid}")
+    public  Customer getcustomerbycid(@PathVariable ("cid") int cid) {
+        Customer customer = customerRepo.findById(cid).get();
+
+        //http://localhost:8002/orders/api/findOrdersByCid/2
+
+        List<Orders> ordersList = restTemplate.getForObject("http://localhost:8002/orders/api/findOrdersByCid/"+cid, List.class);
+
+        customer.setOrdersList(ordersList);
+
+        return customer;
+
+    }
+
 }
